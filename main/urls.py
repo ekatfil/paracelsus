5d5c6b8e46dd68aa946aca9ecb0d8b18f3cae45c
ekatfil <ekatfil@mail.ru>
2023-11-11 12:20:33 +0300

    Revert "cteate_user, delete_user, edit_user does not work"
    This reverts commit c30810547b5524a76dfe05a0299b22adfe34c203.

--- main/urls.py
+++ main/urls.py
@@ -7,6 +7,6 @@ app_name = "main"
 urlpatterns = [
     path("", main.index, name="index"),
     path("create_user/", main.create_user),
-    path("edit_user/<int:id>/", main.edit_user, name="edit_user"),
+    path("edit/<int:id>/", main.edit),
     path("delete/<int:id>/", main.delete),
 ]
--- main/views.py
+++ main/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponseNotFound
 from .models import User
-from django.http import JsonResponse
-from django.shortcuts import get_object_or_404
 
 # Create your views here.
 
@@ -25,29 +23,21 @@ def create_user(request):
         user.password = request.POST.get("password")
         user.save()
     return HttpResponseRedirect("/")    
-    
+        
 
-
-def edit_user(request, id):
-    if request.method == "POST":
-        user = get_object_or_404(User, id=id)
-        user.name = request.POST.get("editName")
-        user.lastname = request.POST.get("editLastname")
-        user.email = request.POST.get("editEmail")
-        user.save()
-        return JsonResponse({'success': True})
-
-
-def get_user_data(request, id):
-    user = get_object_or_404(User, id=id)
-    data = {
-        'name': user.name,
-        'lastname': user.lastname,
-        'email': user.email,
-    }
-    return JsonResponse({'success': True, 'user': data})
-   
-    
+def edit(request, id):
+    try:
+        user = User.objects.get(id=id)
+ 
+        if request.method == "POST":
+            user.name = request.POST.get("name")
+            user.lastname = request.POST.get("lastname")
+            user.email = request.POST.get("email")
+            user.password = request.POST.get("password")
+            user.save()
+            return HttpResponseRedirect("/")
+    except User.DoesNotExist:
+        return HttpResponseNotFound("<h2>User not found</h2>")
     
 
 

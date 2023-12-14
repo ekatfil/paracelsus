09caf06297d6f49400c0c935d3181959c9cd50bb
ekatfil <ekatfil@mail.ru>
2023-12-15 00:09:19 +0300

    fix calendar modals

--- static/js/modal.js
+++ static/js/modal.js
@@ -1,31 +1,46 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // ... your existing code ...
-
-    // Get modal elements
     const noteModal = document.getElementById("noteModal");
-    const closeModalBtn = document.querySelector(".close");
+    const closeModalBtn = document.querySelector(".close-notes-modal");
+    const createnoteModal = document.getElementById("createnoteModal");
+    const addNoteBtn = document.getElementById("add-note");
+    const saveNoteBtn = document.getElementById("save-note");
+    const calendarTable = document.getElementById("calendarTable");
+    const closeCreateNoteModalBtn = createnoteModal.querySelector(".close-create-modal");
 
-    // Open the modal when a day is clicked
     calendarTable.addEventListener("click", function (event) {
         const clickedElement = event.target;
         if (clickedElement.tagName === "TD") {
-            // You may want to implement logic to identify the specific day clicked
-            // For simplicity, let's assume you have a function getDayFromDate() that returns the day
-
-            // Open the modal here
-            noteModal.style.display = "block";
+            noteModal.classList.add("visible");
         }
     });
 
-    // Close the modal when the close button is clicked
     closeModalBtn.addEventListener("click", function () {
-        noteModal.style.display = "none";
+        noteModal.classList.remove("visible");
     });
 
-    // Close the modal if the user clicks outside of it
+    addNoteBtn.addEventListener("click", function () {
+        noteModal.classList.remove("visible");
+        createnoteModal.classList.add("visible");
+    });
+
+    saveNoteBtn.addEventListener("click", function () {
+        createnoteModal.classList.remove("visible");
+        noteModal.classList.add("visible");
+    });
+
+    closeCreateNoteModalBtn.addEventListener("click", function () {
+        createnoteModal.classList.remove("visible");
+        noteModal.classList.add("visible");
+    });
+
     window.addEventListener("click", function (event) {
         if (event.target === noteModal) {
-            noteModal.style.display = "none";
+            noteModal.classList.remove("visible");
+        }
+        if (event.target === createnoteModal) {
+            createnoteModal.classList.remove("visible");
+            noteModal.classList.add("visible"); 
         }
     });
 });
+
